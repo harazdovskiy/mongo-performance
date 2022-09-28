c6302701aab03de6b6b5d6f6054c5292193c9bcc
harazdovskiy <harazdovskiy@gmail.com>
2022-09-29 00:40:23 +0300

    feat: Removed clutter from reading reviews count script

--- insert/50m+/read-size-of-dataset/index.js
+++ insert/50m+/read-size-of-dataset/index.js
@@ -3,6 +3,8 @@ const {from = 0, to = 1} = require('minimist')(process.argv.slice(2));
 const radash = require('radash');
 
 const BASE_PATH = '../dataset/reviews';
+const PARALLEL_EXECUTIONS = 5;
+const LOG_PATH_EVERY = 1_000_000;
 
 (async () => {
     let started = Date.now()
@@ -10,30 +12,29 @@ const BASE_PATH = '../dataset/reviews';
 
     const workingPaths = dirsPaths.slice(+from, +to).filter(path => !path.startsWith('.'))
     let doneReadingReviews = 0;
-    console.log({workingPaths})
-    let donePaths = workingPaths.length;
+    console.log('\n All paths', workingPaths.length)
+    let donePaths = 0;
+    let moreThan = LOG_PATH_EVERY;
     for (const path of workingPaths) {
 
-
         const paginationDirPath = `${BASE_PATH}/${path}`;
-        console.log(`reading ${paginationDirPath}  donePaths - ${donePaths} done reviews ${doneReadingReviews}`)
-
-
         const innerPaths = await fs.promises.readdir(paginationDirPath);
-        // console.log('innerPaths ', innerPaths)
-        await radash.parallel(5, innerPaths, async (path) => {
+
+        await radash.parallel(PARALLEL_EXECUTIONS, innerPaths, async (path) => {
             const filePath = `${paginationDirPath}/${path}`;
-            // console.log('inp', filePath)
+
             const chunk = await fs.promises.readFile(filePath);
-            const reviews = JSON.parse(new Buffer(chunk).toString()).reviews || [];
+            const reviews = JSON.parse(Buffer.from(chunk).toString()).reviews || [];
             doneReadingReviews += reviews.length;
+
+            if (doneReadingReviews > moreThan) {
+                console.log(`done paths - ${donePaths} done reviews ${doneReadingReviews}`)
+                moreThan += LOG_PATH_EVERY
+            }
         })
 
-        donePaths--;
+        donePaths++;
     }
-
-
     console.log('Reviews', doneReadingReviews)
-
     console.log('Operation took - ', (Date.now() - started) * 0.001, ' seconds\n');
 })()
